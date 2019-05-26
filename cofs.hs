0a2d4d763f247308c2b1399841cb6f52087fbde5
Axel Ramge <axel@ramge.de>
2019-05-26 14:41:07 +0200

    a little bit cleaner

--- cofs.hs
+++ cofs.hs
@@ -1,24 +1,30 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE TypeFamilies              #-}
-{-# LANGUAGE ParallelListComp          #-}
 
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import Data.Colour (withOpacity)
 
+startRadius = 3.0
+padding = 0.1
+
 -- Ring of Keys; Spiral of 19 Keys in Wedges ------------------------------
-keyText = [       "Cb", "Gb", "Db", "Ab", "Eb", "Bb",
-            "F",  "C",  "G",  "D",  "A",  "E",  "B",
-            "F#", "C#", "G#", "D#", "A#", "E#"]
+keyText = [
+  -- "...", "Bbb", "Fb",
+  "Cb", "Gb", "Db", "Ab", "Eb", "Bb",
+  "F",  "C",  "G",  "D",  "A",  "E",  "B",
+  "F#", "C#", "G#", "D#", "A#", "E#"
+  -- ,"B+", "..."
+  ]
 key :: String -> Diagram B
 key s = text s <> square 1 # lw none
 keysRing :: Diagram B
-keysRing = mconcat [ key s # translateY t # rotateBy (-n/12)
-                | s <- keyText
-                | n <- [1..]
-                | t <- [3, 3.07..]] -- !!! Delta should be found automaticall from fontSize/12 + padding
-
+spiralDelta = (0.8 + padding) / 12.0
+keysRing = mconcat
+  . zipWith rotateBy [-1/12, -2/12..]
+  . zipWith translateY [startRadius, (startRadius+spiralDelta)..]
+  $ map key keyText
 aKeyWedge :: Diagram B
 aKeyWedge = annularWedge 5 2.6 xDir (1/12 @@ turn) # rotateBy (-1/24)
 keyWedges :: Diagram B
@@ -27,41 +33,34 @@ keyWedges = mconcat . take 12 . iterate (rotateBy (1/12)) $ aKeyWedge
 -- Ring Solfege; 7 Solfege Syllables in colored wedges (3 major, 3 minor, 1 diminished)
 solfegeText = ["Fa", "Do", "So", "re", "la", "mi", "TI"]
               -- could also be prolongued with "Fi", "Di" and so on
-solfegeRing = mconcat [ key s # translateY 5.5 # rotateBy (-n/12)
-                      | s <- solfegeText
-                      | n <- [1..]
-                      ]
-
+solfegeRing = mconcat
+  . zipWith rotateBy [-1/12, -2/12..]
+  . map (translateY 5.5)
+  $ map key solfegeText
 modesText = ["Lydian", "Ionian", "Mixolydian", "Dorian", "Aeolian", "Phrygian", "Locrian"]
-modesRing  = mconcat [ key s # translateY 6.4 # rotateBy (-n/12) # fontSize (local 0.6)
-                      | s <- modesText
-                      | n <- [1..]
-                      ]
-
+modesRing = mconcat
+  . zipWith rotateBy [-1/12, -2/12..]
+  . map (translateY 6.4 # fontSize (local 0.6))
+  $ map key modesText
 solfegeColors =  [honeydew, honeydew, honeydew, bisque, bisque, bisque, pink]
 aSolfegeWedge :: Diagram B
 aSolfegeWedge = annularWedge 7 5.1 yDir (1/12 @@ turn) # rotateBy (-1/24)
 solfegeWedges :: Diagram B
-solfegeWedges = mconcat [ aSolfegeWedge
-                          # rotateBy (-n/12)
-                          # fc c
-                        | n <- [1..]
-                        | c <- solfegeColors
-                        ]
+solfegeWedges = mconcat
+  . zipWith rotateBy [-1/12, -2/12..]
+  . zipWith fc solfegeColors
+  $ repeat aSolfegeWedge
 -- -------------------------------------------------------------------------
 -- Ring Steps; 13 Steps in 12 Wedges; the "7" wedge is colored
 stepsText = ["III-IV", "I", "V", "II", "VI", "III", "VII", "IV", "I", "V", "II", "VI"]
-stepsRing =  mconcat [ key s # translateY 7.6 # rotateBy (-n/12)
-                      | s <- stepsText
-                      | n <- [1..]
-                      ]
+stepsRing = mconcat
+  . zipWith rotateBy [-1/12, -2/12..]
+  . map (translateY 7.6 # fontSize (local 1.0))
+  $ map key stepsText
 aStepWedge :: Diagram B
 aStepWedge = annularWedge 8.3 7.1 yDir (1/12 @@ turn) # rotateBy (-1/24)
 stepWedges :: Diagram B
-stepWedges = mconcat [ aStepWedge 
-                       # rotateBy (-n/12)
-                     | n <- [1..12]
-                     ]
+stepWedges = mconcat . take 12 . iterate (rotateBy (1/12)) $ aStepWedge
 -- -------------------------------------------------------------------------
 cofs :: Diagram B
 cofs = circle 0.1 <> keysRing <> keyWedges <> solfegeRing <> modesRing <> solfegeWedges <> stepsRing <> stepWedges
